9056f7a74a57844fc788af924e2726247f551711
mallquidev <159455849+mallquidev@users.noreply.github.com>
2024-06-16 19:21:05 -0500

    config

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const {
+    PORT = 3000,
+    SECRET_JWT_KEY = 'scret123'
+} = process.env
--- src/controllers/auth.controlles.js
+++ src/controllers/auth.controlles.js
@@ -1,11 +1,17 @@
 import { pool } from '../db.js'
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import {SECRET_JWT_KEY} from '../config.js'
 
 export const register = async (req, res) => {
     try {
         const { email, username, password } = req.body
         const passwordHash = await bcrypt.hash(password, 10)
         const [result] = await pool.query('INSERT INTO user(email, username, password) VALUES(?,?,?)', [email, username, passwordHash])
+
+        //jwt
+        const token = jwt.sign({id: result.insertId}, SECRET_JWT_KEY)
+
         res.send(result)
     } catch (error) {
         res.status(500).json({ message: error.message })
