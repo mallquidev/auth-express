2c6a3a03d61b6103a46184039641fad1792f1001
mallquidev <159455849+mallquidev@users.noreply.github.com>
2024-07-02 09:27:40 -0500

    logout token finished

--- src/controllers/auth.controlles.js
+++ src/controllers/auth.controlles.js
@@ -39,3 +39,14 @@ export const login = async(req, res) => {
         res.status(500).json({ message: 'Internal Server Error' });
     }
 }
+
+export const logout = (req, res) => {
+    try {
+        res.cookie('token', '',{
+            expires: new Date(0)
+        })
+        return res.sendStatus(200)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
