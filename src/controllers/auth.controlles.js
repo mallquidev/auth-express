0fff0b126349f178c2a29f8de60e191ab90cf860
mallquidev <159455849+mallquidev@users.noreply.github.com>
2024-07-02 09:19:58 -0500

    login token finished

--- src/controllers/auth.controlles.js
+++ src/controllers/auth.controlles.js
@@ -20,4 +20,22 @@ export const register = async (req, res) => {
         res.status(500).json({ message: error.message })
     }
 }
-export const login = (req, res) => res.send('login')
+export const login = async(req, res) => {
+    try {
+        const {email, password} = req.body
+        const [userFound] = await pool.query('SELECT * FROM user WHERE email = ?', [email])
+        if(userFound.length === 0) return res.status(404).json({message: 'Correo no encontrado'})
+        const user = userFound[0]
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch) return res.status(203).json({message: 'Unauthorized'})
+        
+        const token = await createAccessToken({id: userFound.insertId})
+        res.cookie('token', token)
+        res.json(token)
+        
+    } catch (error) {
+        // Manejo del error
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
